cd4f90f69215954239731b79351eebd71bb7a313
Ian Eldridge-Allegra <ike@duke.edu>
2017-10-30 23:20:52 -0400

    MORE comments

--- src/model/TurtleManagerSpecificCommand.java
+++ src/model/TurtleManagerSpecificCommand.java
@@ -11,6 +11,9 @@ package model;
 public abstract class TurtleManagerSpecificCommand implements Command {
 	private static final long serialVersionUID = 4234355250566310877L;
 
+	/* (non-Javadoc)
+	 * @see model.Command#execute(model.Turtle, model.CommandManager, model.VariableManager)
+	 */
 	@Override
 	public double execute(Turtle t, CommandManager commands, VariableManager variables) {
 		if (t instanceof TurtleManager)
@@ -18,6 +21,14 @@ public abstract class TurtleManagerSpecificCommand implements Command {
 		throw new SLogoException("NestedCommand");
 	}
 
+	/**
+	 * The same as execute, but now t is guaranteed to be a TurtleManager
+	 * 
+	 * @param t A TurtleManager to execute commands on
+	 * @param commands Known commands
+	 * @param variables Known variables
+	 * @return The result of the command
+	 */
 	protected abstract double executeOnManager(TurtleManager t, CommandManager commands, VariableManager variables);
 
 }
